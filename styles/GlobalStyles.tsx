6e3d2e63c08534b5aeb37ecd1def6afa9454a233
geonhwiii <g01063962671@gmail.com>
2023-01-11 18:57:35 +0900

    apply basic styling

--- styles/GlobalStyles.tsx
+++ styles/GlobalStyles.tsx
@@ -1,11 +1,34 @@
 import {Global} from '@emotion/react';
-import tw, {css, theme, GlobalStyles as BaseStyles} from 'twin.macro';
+import tw, {css, GlobalStyles as BaseStyles} from 'twin.macro';
 
 const customStyles = css({
-  body: {
-    background: theme`colors.white`,
-    WebkitTapHighlightColor: theme`colors.purple.500`,
+  html: {
+    ...tw`w-full`,
+    ...tw`[font-size: 16px]`,
+    ...tw`[line-height: 1.75]`,
+    ...tw`[text-size-adjust: none]`,
     ...tw`antialiased`,
+  },
+  body: {
+    ...tw`bg-white`,
+    ...tw`w-full`,
+    ...tw`antialiased`,
+    ...tw`[word-break: keep-all]`,
+    ...tw`[color: #111]`,
+  },
+  'html, body, #__next': {
+    ...tw`min-h-full`,
+    ...tw`[font-family:"Noto Sans KR","Arial Bold","Helvetica Neue",Helvetica,Arial,Dotum,Gulim,sans-serif]`,
+  },
+  '#__next': {
+    ...tw`relative`,
+  },
+  '*': {
+    ...tw`box-border`,
+  },
+  a: {
+    ...tw`[color: inherit]`,
+    ...tw`[text-decoration: non]`,
   },
 });
 
